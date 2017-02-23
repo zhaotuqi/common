30515cd45798936c0e033f597c6c318ff3483b88
shaobaojie <baojie.shao@wenba100.com>
2017-02-23 21:04:48 +0800

    common

--- src/Libraries/Common.php
+++ src/Libraries/Common.php
@@ -15,6 +15,8 @@ use RuntimeException;
 
 class Common
 {
+    const EACH_PAGE_NUM = 10000;
+
     public function convertString(array $param)
     {
         return array_map(function ($v) {
@@ -30,6 +32,15 @@ class Common
     {
         $data = array_map(function ($v) {
             return iconv('gbk', 'utf-8', $v);
+        }, $data);
+
+        return $data;
+    }
+
+    public function convertGBK($data)
+    {
+        $data = array_map(function ($v) {
+            return iconv('utf-8', 'GBK', $v);
         }, $data);
 
         return $data;
@@ -190,6 +201,54 @@ class Common
     }
 
     /**
+     * request POST FORM DATA
+     * @param $requestUrl
+     * @param $param
+     * @return mixed
+     */
+    public function requestMultipart($requestUrl, $multipart, $headers = [])
+    {
+        $httpClient = app('HttpClient');
+
+        try {
+            $i = 0;
+            request:
+            $result = $httpClient->request('POST', $requestUrl,
+                [
+                    'multipart'       => $multipart,
+                    'headers'         => $headers,
+                    'timeout'         => 3,
+                    'connect_timeout' => 3
+                ])->getBody()->getContents();
+
+        } catch (RuntimeException $e) {
+            if ($i < 5) {
+                $i++;
+                goto request;
+            } else {
+                throw $e;
+            }
+        }
+
+        $message = [
+            'request_uri'    => $requestUrl,
+            'request_header' => $headers,
+            'request_body'   => $multipart,
+            'response_body'  => @json_decode($result, true) ?: $result
+        ];
+
+        //记录log
+        $this->logger(
+            config('app.app_name'),
+            'servicelog.log',
+            json_encode($message, JSON_UNESCAPED_UNICODE),
+            Logger::INFO
+        );
+
+        return $result;
+    }
+
+    /**
      * 发送json raw data 请求
      * @param $requestUrl
      * @param $param
@@ -242,5 +301,57 @@ class Common
         ];
 
         return array_merge($array, $param, $list);
+    }
+
+    public function getOrderId($prefix = null)
+    {
+        return $prefix . date('YmdHis') . substr(base_convert(uniqid(), 16, 10), -4) . str_pad(mt_rand(1, 9999), 4, '0', STR_PAD_LEFT);
+    }
+
+    //显示数据
+    public function getClockByTimestamp($time)
+    {
+        $time = $time - 28800;//减去八小时的秒
+        $date = date("H:i", $time);
+        if ($time > 0 && $date == '00:00') {
+            return '24:00';
+        }
+
+        return $date;
+    }
+
+    public function getWeekName($index)
+    {
+        $week = ['周日', '周一', '周二', '周三', '周四', '周五', '周六'];
+        if ($index > count($week) || $index < 0) {
+            return '周日';
+        }
+
+        return $week[$index];
+    }
+
+    /**
+     * 输出为excel
+     * @param  {[type]} $arrCellName    [description]
+     * @param  {[type]} $arrListData    [description]
+     * @param  {[type]} $strFileNamePre [description]
+     * @return {[type]}                 [description]
+     */
+    public function ouputAsExcel($arrCellName, $arrListData, $strFileNamePre)
+    {
+        Excel::create($strFileNamePre . time(), function ($excel) use ($arrListData, $arrCellName, $strFileNamePre) {
+            $intSheetNum = ceil(count($arrListData) / self::EACH_PAGE_NUM);
+            for ($index = 0; $index < $intSheetNum; $index++) {
+                $arrCellDataTmp = $arrCellName;
+                $arrTmpInfo = array_slice($arrListData, $index * self::EACH_PAGE_NUM, self::EACH_PAGE_NUM);
+                foreach ($arrTmpInfo as $singleTmpInfo) {
+                    $arrCellDataTmp[] = $singleTmpInfo;
+                }
+                $strSheetName = sprintf("%s_%d", $strFileNamePre, $index);
+                $excel->sheet($strSheetName, function ($sheet) use ($arrCellDataTmp) {
+                    $sheet->rows($arrCellDataTmp);
+                });
+            }
+        })->export('xls');
     }
 }
