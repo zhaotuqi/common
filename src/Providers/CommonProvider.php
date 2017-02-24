443781212b17621a8cb9252d4b50f3fb53b421b1
shaobaojie <baojie.shao@wenba100.com>
2017-02-24 10:27:49 +0800

    production

--- src/Providers/CommonProvider.php
+++ src/Providers/CommonProvider.php
@@ -16,14 +16,13 @@ class CommonProvider extends ServiceProvider
     {
         //
         $this->publishes([__DIR__ . '/../common_config.php' => config_path('common_config.php')]);
-        $content = file_get_contents(base_path('.env'));
-        if (false === strrpos($content, 'WARNING_EMAIL_URL')) {
-            if (App()->environment() == 'production') {
-                file_put_contents(base_path('.env'), 'WARNING_EMAIL_URL=http://10.10.146.223/mail/sendWarning', FILE_APPEND);
-            } else {
-                file_put_contents(base_path('.env'), 'WARNING_EMAIL_URL=http://10.1.1.100:8299/mail/sendWarning', FILE_APPEND);
+        if (!config('common_config.WARNING_EMAIL_URL')) {
+                if (App()->environment() == 'production') {
+                    file_put_contents(base_path('.env'), 'WARNING_EMAIL_URL=http://10.10.146.223/mail/sendWarning', FILE_APPEND);
+                } else {
+                    file_put_contents(base_path('.env'), 'WARNING_EMAIL_URL=http://10.1.1.100:8299/mail/sendWarning', FILE_APPEND);
+                }
             }
-        }
     }
 
     /**
